7ea8966f301b6fd02a6d0148969636776f0222ff
Mako88 <afuturepilotis@gmail.com>
2021-12-09 17:17:07 -0600

    Initial Commit

---
+++ Menu.cs
@@ -0,0 +1,12 @@
+﻿using Sharprompt;
+
+namespace TextRPG
+{
+    public abstract class Menu
+    {
+        public string Display(string prompt, List<string> options)
+        {
+            return Prompt.Select(prompt, options);
+        }
+    }
+}
